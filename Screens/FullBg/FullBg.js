14068eacf5da3354ec3afd5eab728b465bbcac82
justanothermanoj <justanothermanoj@gmail.com>
2016-09-28 22:09:19 +0530

    added opacity

--- Screens/FullBg/FullBg.js
+++ Screens/FullBg/FullBg.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, StyleSheet, View } from 'react-native';
+import Colors from '../../Style/colors';
 
 class FullBg extends Component {
 	render() {
 		return (
-			<Image source={require('../../assets/bg.jpg')}></Image>
+			<Image source={require('../../assets/bg.jpg')} style={styles.bg}>
+				<View style={styles.cover}>
+				</View>
+			</Image>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	bg: {
+		flex: 1,
+	},
+	cover: {
+		flex: 1,
+		backgroundColor: Colors.red,
+		opacity: 0.8
+	}
+});
+
+
 
 export default FullBg;
